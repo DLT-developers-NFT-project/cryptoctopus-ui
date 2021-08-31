08ae9262a3e3f2d219a789d5027ccdf7b02761b8
GigaHierz <lenahierzi@hotmail.de>
2021-08-31 17:42:05 +0200

    Add Welcome component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+
+    <Welcome
+      msg="Welcome to DLT Talents NFT Game!
+Please, enter your name to claim your NFT"
+    />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
+import Welcome from "@/components/Welcome.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    Welcome,
+    // HelloWorld,
   },
 };
 </script>
